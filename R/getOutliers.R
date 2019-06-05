e09deb67b58052cfb3cfb283245ad7c02a1c9599
Kristian Kleinke <kkleinke@Kristians-MBP-2.fritz.box>
2019-06-05 10:13:48 +0200

    fixed problem with dependency extremevalues

--- R/getOutliers.R
+++ R/getOutliers.R
@@ -1,4 +1,4 @@
-# note: function getOutliers was imported from package extremevalues 2.3.2 2016-01-05
+# note: functions were imported from package extremevalues 2.3.2 2016-01-05
 getOutliers<-function (y, rho = c(1, 1), FLim = c(0.1, 0.9), distribution = "normal") 
 {
   if (!is.vector(y)) 
@@ -34,4 +34,46 @@ getOutliers<-function (y, rho = c(1, 1), FLim = c(0.1, 0.9), distribution = "nor
   out$Fmin = FLim[1]
   out$Fmax = FLim[2]
   return(out)
+}
+
+getNormalLimit<-function (y, p, N, rho) 
+{
+  param <- fitNormal(y, p)
+  ell <- c(Left = -Inf, Right = Inf)
+  if (!is.na(rho[1])) 
+    ell[1] <- sqrt(2) * param$sigma * invErf(2 * rho[1]/N - 
+                                               1) + param$mu
+  if (!is.na(rho[2])) 
+    ell[2] <- sqrt(2) * param$sigma * invErf(1 - 2 * rho[2]/N) + 
+    param$mu
+  return(list(mu = param$mu, sigma = param$sigma, nFit = length(y), 
+              R2 = param$R2, limit = ell))
+}
+
+invErf <- function (x) 
+{
+  if (sum(x >= 1) > 0 | sum(x <= -1) > 0) 
+    stop("Argument must be between -1 and 1")
+  return(qnorm((1 + x)/2)/sqrt(2))
+}
+
+fitNormal<-function (y, p) 
+{
+  if (!is.vector(y)) 
+    stop("First argument is not of type vector")
+  if (!is.vector(p)) 
+    stop("First argument is not of type vector")
+  if (sum(p <= 0) > 0 | sum(p >= 1) > 0) 
+    stop("Second argument contains values out of range (0,1)")
+  if (length(y) != length(p)) 
+    stop("First and second argument have different length")
+  N <- length(y)
+  Y <- as.matrix(y, nrow = N)
+  p <- as.matrix(p, nrow = N)
+  A <- matrix(0, nrow = N, ncol = 2)
+  A[, 1] <- 1 + double(N)
+  A[, 2] <- sqrt(2) * invErf(2 * p - 1)
+  param <- solve(t(A) %*% A) %*% t(A) %*% Y
+  r2 <- 1 - var(A %*% param - y)/var(y)
+  return(list(mu = param[1], sigma = param[2], R2 = r2))
 }
